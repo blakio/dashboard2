eaff6cfd968e4383b263312633d87ece8b6f299d
Isaiah Harrison <ijh5005@outlook.com>
2019-11-16 03:36:48 -0500

    test

---
+++ build/precache-manifest.c21218c77fdf0d1adcccfcc60951d3d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d20ae5115833370c3d70701d8f2af09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2829dfee178a9e98f4d8",
+    "url": "/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "ab6cc5ae92c62c3c9535",
+    "url": "/static/css/main.a765fa66.chunk.css"
+  },
+  {
+    "revision": "2829dfee178a9e98f4d8",
+    "url": "/static/js/2.7c47dad7.chunk.js"
+  },
+  {
+    "revision": "ab6cc5ae92c62c3c9535",
+    "url": "/static/js/main.1309ebdf.chunk.js"
+  },
+  {
+    "revision": "bd648bced68b514521ea",
+    "url": "/static/js/runtime-main.4c5795d0.js"
+  },
+  {
+    "revision": "b6e789daae846d0c45f303aa7348d603",
+    "url": "/static/media/cwlogo.b6e789da.png"
+  }
+]);
